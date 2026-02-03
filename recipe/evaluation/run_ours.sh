4726d0e04a0d3fe372e3d3f4d93c75f395124928
MikaStars39 <meloxdmy@qq.com>
2026-02-03 22:15:09 +0800

    [scripts] running scripts

--- recipe/evaluation/run_ours.sh
+++ recipe/evaluation/run_ours.sh
@@ -13,13 +13,13 @@ export PYTHONFAULTHANDLER=1
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 PROJECT_DIR=/mnt/llm-train/users/explore-train/qingyu/slime
-ORIGINAL_PATH=/mnt/llm-train/users/explore-train/qingyu/ckpt/20260202_142135_langauge_rl/iter_0000335
+ORIGINAL_PATH=/mnt/llm-train/users/explore-train/qingyu/ckpt/20260203_103651_langauge_rl_new_0203_v2_sft/iter_0000031
 
 PYTHONPATH=/root/Megatron-LM python \
     ${PROJECT_DIR}/tools/convert_torch_dist_to_hf.py \
     --input-dir $ORIGINAL_PATH \
     --output-dir ${ORIGINAL_PATH}_hf \
-    --origin-hf-dir /mnt/llm-train/users/explore-train/qingyu/ckpt/sft-16k-v1/12524 \
+    --origin-hf-dir /mnt/llm-train/users/explore-train/qingyu/ckpt/sft-16k-v2 \
     --vocab-size 129280 \
     --chunk-size 10000000000
 
@@ -28,19 +28,19 @@ MODEL_PATH=${ORIGINAL_PATH}_hf
 MODEL_PATH=/mnt/llm-train/users/explore-train/wangzhenfang8/output/40b/sft-postrain-exp-v8-baselong-e2_lr5e-5_minlr5e-6/50099/
 CACHE_DIR="/mnt/llm-train/users/explore-train/qingyu/.cache"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-OUTPUT_DIR="/mnt/llm-train/users/explore-train/qingyu/data/eval_outputs/${TIMESTAMP}_ours_12524"
+OUTPUT_DIR="/mnt/llm-train/users/explore-train/qingyu/data/eval_outputs/${TIMESTAMP}_ours_50099"
 
 # Step 1: Prepare data (load benchmarks and apply chat template)
-python /mnt/llm-train/users/explore-train/qingyu/OpenGym/recipe/evaluation/prepare_data.py \
-    --dataset "ifeval@1" \
+python /mnt/llm-train/users/explore-train/qingyu/slimulation/recipe/evaluation/prepare_data.py \
+    --dataset "gpqa_diamond@8" \
     --cache-dir "$CACHE_DIR" \
     --out-dir "$OUTPUT_DIR" \
     --model "$MODEL_PATH" \
-    --prompt-format "blank" \
+    --prompt-format "lighteval" \
     --system-prompt "You are JoyAI, a large language model trained by JD (京东). Answer as concisely as possible."
 
 # Step 2: Run batch inference
-python /mnt/llm-train/users/explore-train/qingyu/OpenGym/recipe/evaluation/inference.py \
+python /mnt/llm-train/users/explore-train/qingyu/slimulation/recipe/evaluation/inference.py \
     --input "$OUTPUT_DIR/data.chat.jsonl" \
     --output "$OUTPUT_DIR/results.jsonl" \
     --model "$MODEL_PATH" \
@@ -52,7 +52,7 @@ python /mnt/llm-train/users/explore-train/qingyu/OpenGym/recipe/evaluation/infer
     --resume
 
 # Step 3: Evaluate and calculate metrics
-python /mnt/llm-train/users/explore-train/qingyu/OpenGym/recipe/evaluation/evaluate.py \
+python /mnt/llm-train/users/explore-train/qingyu/slimulation/recipe/evaluation/evaluate.py \
     --input "$OUTPUT_DIR/results.jsonl" \
     --output-dir "$OUTPUT_DIR" \
     --num-proc 32
